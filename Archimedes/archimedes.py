eba57bebd91019bd7a0b5f4409e9808e5699ab0b
John Hancock <jhancock1975@gmail.com>
2017-06-12 02:31:14 +0000

    work on Archimedies' method.

--- Archimedes/archimedes.py
+++ Archimedes/archimedes.py
@@ -1,3 +1,25 @@
 # Python implementation of
 # Archimedes' method for
 # estimating pi
+class arch:
+	__a0 = 2*(3**0.5)
+	__b0 = 3
+	
+	def __an(self, n):
+		if (n == 0):
+			return arch.__a0
+		else:
+			return (2*self.__an(n-1)*self.__bn(n-1))/(self.__an(n-1)+self.__bn(n-1))
+	
+	def __bn(self, n):
+		if (n == 0):
+			return arch.__b0
+		else:
+			return (self.__an(n)*self.__bn(n-1))**0.5
+
+	def arch_mean_iter(self, n):
+		return self.__an(n)
+
+a = arch()
+a.arch_mean_iter(4.0)
+
